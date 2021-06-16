cdba27aaa3aeed84d0399b04c9e1ec83fe1ea559
Igor Fonseca <igorfonsecaigor@gmail.com>
2021-06-16 09:25:51 -0300

    Atualizando versão do código principal

--- main.cpp
+++ main.cpp
@@ -1,64 +1,151 @@
-#include <ogdf/fileformats/GraphIO.h>
-#include <ogdf/orthogonal/OrthoLayout.h>
-#include <ogdf/planarlayout/MixedModelLayout.h>
-#include <ogdf/planarity/EmbedderMinDepthMaxFaceLayers.h>
-#include <ogdf/planarity/PlanarSubgraphFast.h>
-#include <ogdf/planarity/PlanarizationLayout.h>
-#include <ogdf/planarity/SubgraphPlanarizer.h>
-#include <ogdf/planarity/VariableEmbeddingInserter.h>
+/* 
+* Autor: Igor Oliveira da Fonseca
+*
+* Alguns trechos adaptados de: https://chameerawijebandara.wordpress.com/2014/03/15/sample-layout-in-ogdf/
+*
+*/
+
+#include <ogdf/basic/Graph.h>
 #include <ogdf/basic/graph_generators.h>
-#include <ogdf/layered/DfsAcyclicSubgraph.h>
+#include <ogdf/planarlayout/MixedModelLayout.h>
 #include <ogdf/fileformats/GraphIO.h>
  
+#include <iostream>
+#include <chrono>
+#include <ctime> 
+#include <string> 
+#include <iostream>
+#include <fstream>
+
 using namespace ogdf;
  
-int main()
+int main(int argc,char* argv[])
 {
+    
+    int i_start = 0;
+    int i_end = 0;
+     if (argc==3) {
+         // Utiliza valores de step e n máximo passados como argumento
+         i_start = std::stoi(argv[1]);
+         i_end = std::stoi(argv[2]);
+     }
+     else {
+         // Não foram passados argumentos, usa valores padrão
+         i_start = 50;
+         i_end = 500;
+     }
 
-    // Grafo comum, sem peso e com lista de adjacência.
-    Graph G;
-    randomSimpleGraph(G, 20, 40);
- 
-    DfsAcyclicSubgraph DAS;
-    DAS.callAndReverse(G);
- 
-    // Adiciona atributos ao grafo
-    GraphAttributes GA(G,
-      GraphAttributes::nodeGraphics | GraphAttributes::nodeType |
-      GraphAttributes::edgeGraphics | GraphAttributes::edgeType);
- 
-    for (node v : G.nodes)
-    {
-        GA.width(v) /= 2;
-        GA.height(v) /= 2;
+    int loop = 1; // contador de loops
+
+    bool is_sparse = true; // controle de grafo esparso/denso
+    
+    // Inicia o arquivo CSV
+    std::ofstream myfile;
+    myfile.open ("output_times.csv");
+    myfile << "0";
+    for (int i = i_start; i <= i_end; i = i + i_start) {
+        myfile << "," << i;
     }
- 
-    PlanarizationLayout pl;
- 
-    // Planariza o grafo para diminuir cruzamentos de arestas
-    SubgraphPlanarizer crossMin;
- 
-    auto* ps = new PlanarSubgraphFast<int>;
-    ps->runs(100);
-    VariableEmbeddingInserter *ves = new VariableEmbeddingInserter;
-    ves->removeReinsert(RemoveReinsertType::All);
- 
-    crossMin.setSubgraph(ps);
-    crossMin.setInserter(ves);
- 
-    EmbedderMinDepthMaxFaceLayers *emb = new EmbedderMinDepthMaxFaceLayers;
-    pl.setEmbedder(emb);
- 
-    // Layout ortogonal para grafos planares
-    OrthoLayout *ol = new OrthoLayout;
-    ol->separation(20.0);
-    ol->cOverhang(0.4);
-    pl.setPlanarLayouter(ol);
- 
-    pl.call(GA);
- 
-    //GraphIO::write(GA, "mygraph.gml", GraphIO::writeGML);
-    GraphIO::write(GA, "mygraph20.svg", GraphIO::drawSVG);
- 
+    myfile << "\n";
+
+    for (int i = i_start; i <= i_end; i = i + i_start) {
+
+        int edges_sparse =  i * 1.5;
+        int edges_dense = (3 * i) - 6;
+
+        // Inicia o timer
+        auto start = std::chrono::system_clock::now();
+
+        // Gera o grafo aleatório
+        Graph G;
+        if (is_sparse)
+            randomPlanarTriconnectedGraph(G, i, edges_sparse);
+        else
+            randomPlanarTriconnectedGraph(G, i, edges_dense);
+
+        // Cria os atributos para o grafo
+        GraphAttributes GA(G, GraphAttributes::nodeGraphics |
+                                    GraphAttributes::edgeGraphics |
+                                    GraphAttributes::nodeLabel |
+                                    GraphAttributes::nodeStyle);
+
+        GA.scale(16, 9, false);
+
+        // Iteração por todos os vértices do grafo
+        node v;
+        for (node v : G.nodes)
+        {
+            GA.fillColor(v) = Color("#13d16e"); // cor de preenchimento do vértice
+
+            GA.height(v) = 20.0;                // define a altura como 20
+            GA.width(v) = 20.0;                 // define a largura como 20
+            GA.shape(v) = ogdf::Shape::Ellipse; // define a forma como elipse
+
+            // Utiliza o índice do vértice como label
+            string s = to_string(v->index());
+            char const *pchar = s.c_str();
+            GA.label(v) = pchar;
+        }
+
+        // Utiliza o algoritmo MixedModelLayout
+        MixedModelLayout MML;
+        MML.call(GA);
+
+        // Gera o grafo em formato SVG
+        auto itos = std::to_string(i);
+        auto title_sparse = "mygraph-" + itos + "-sparse.svg";
+        auto title_dense = "mygraph-" + itos + "-dense.svg";
+        if (is_sparse)
+            GraphIO::write(GA, title_sparse, GraphIO::drawSVG);
+        else
+            GraphIO::write(GA, title_dense, GraphIO::drawSVG);
+
+        // Imprime tempo de processamento
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+        std::cout << "(l=" << loop << ",i=" << i << ") " << elapsed_seconds.count() << "\n";
+
+        // Escreve no arquivo CSV
+        if (i == i_start)
+        {
+            if (is_sparse)
+            {
+                //myfile << "Sparse" << loop << ",0";
+                myfile << "0";
+            }
+            else
+            {
+                //myfile << "Dense" << loop-3 << ",0";
+                myfile << "0";
+            }
+        }
+        myfile << "," << elapsed_seconds.count();
+        if (i == i_end)
+        {
+            myfile << "\n";
+        }
+
+        // Três loops para esparso e três para denso
+        if (i == i_end)
+        { // Terminou este loop
+            if (loop == 3)
+            {
+                // Se terminou o 3º loop, altera para grafo denso
+                is_sparse = false;
+            }
+            i = 0;
+            if (loop == 6)
+            {
+                // Se terminou o 6º loop, permite encerrar
+                i = i_end;
+            }
+            loop++;
+        }
+    }
+
+    // Encerra o arquivo CSV
+    myfile.close();
+
     return 0;
 }
