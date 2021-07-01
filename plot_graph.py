21eeafdc21160e05b6a4d4ee0dba84c27a3768d3
Igor Fonseca <igorfonsecaigor@gmail.com>
2021-06-30 21:47:54 -0300

    Atualizando legenda do grafico

--- plot_graph.py
+++ plot_graph.py
@@ -33,8 +33,8 @@ mean_dense = dense_matrix.mean(0)
 std_dense = dense_matrix.std(0)
 
 # Print array
-'''np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
-print(n_sizes)'''
+#np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
+#print(n_sizes)
 
 # Plot dense
 plt.errorbar(n_sizes, mean_dense, std_dense, color="blue", label='Denso medido', linestyle='', fmt=".")
@@ -44,7 +44,8 @@ range_multiplier = 1
 parametros, pcov = opt.curve_fit(funcao_linear, xdata=n_sizes, ydata=mean_dense)
 tamanhos_aproximados = range(n_sizes[-1] * range_multiplier)
 aproximados = [funcao_linear(x, *parametros) for x in tamanhos_aproximados]
-plt.plot(tamanhos_aproximados, aproximados, label="Denso (aprox.)", color="#7373ff", linestyle="dotted")
+label_dense = "f(n) = n * {:f}".format(parametros[0])
+plt.plot(tamanhos_aproximados, aproximados, label=label_dense, color="#7373ff", linestyle="dotted")
 
 # Plot sparse
 plt.errorbar(n_sizes, mean_sparse,std_sparse, color="red", label='Esparso medido', linestyle='', fmt="^")
@@ -53,12 +54,13 @@ plt.errorbar(n_sizes, mean_sparse,std_sparse, color="red", label='Esparso medido
 parametros, pcov = opt.curve_fit(funcao_linear, xdata=n_sizes, ydata=mean_sparse)
 tamanhos_aproximados = range(n_sizes[-1] * range_multiplier)
 aproximados = [funcao_linear(x, *parametros) for x in tamanhos_aproximados]
-plt.plot(tamanhos_aproximados, aproximados, label="Esparso (aprox.)", color="#ff7373", linestyle="dotted")
+label_sparse = "g(n) = n * {:f}".format(parametros[0])
+plt.plot(tamanhos_aproximados, aproximados, label=label_sparse, color="#ff7373", linestyle="dotted")
 
 # Plot final config
 plt.xlabel('Tamanho da entrada (n)')
 plt.ylabel('Tempo de processamento (s)')
 plt.title('Graph Drawing Algorithm - Igor O. Fonseca')
-#plt.grid(linestyle = '--')
+plt.grid(linestyle = 'dotted')
 plt.legend()
 plt.show()
